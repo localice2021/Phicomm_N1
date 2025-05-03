c80688ae70cc7af9f4f29ef6ea117df9698ff0f5
localice2021 <94673672+localice2021@users.noreply.github.com>
2025-05-03 12:11:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -56,9 +56,9 @@ git clone https://github.com/kenzok8/small package/small
 # rm -rf package/open-packages/luci-app-filebrowser
 # git clone https://github.com/xiaozhuai/luci-app-filebrowser.git package/luci-app-filebrowser
 # git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
-git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
-git clone https://github.com/cokebar/openwrt-vlmcsd.git package/openwrt-vlmcsd
-git clone https://github.com/zhengmz/luci-app-zerotier.git package/luci-app-zerotier
+# git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
+# git clone https://github.com/cokebar/openwrt-vlmcsd.git package/openwrt-vlmcsd
+# git clone https://github.com/zhengmz/luci-app-zerotier.git package/luci-app-zerotier
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
